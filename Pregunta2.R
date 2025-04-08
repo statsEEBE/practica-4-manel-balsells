31a1fb5dbd48c4fac982c15a64c576c06766fed5
manel-balsells <manel.balsells@estudiantat.upc.edu>
2025-04-08 09:36:34 +0200

    Solució Pregunta2.R

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,25 @@
+# Lambda si et donen el valor esperado, es a dir la media, lambda es 1/media
+# Lambda si et donen V(T) = 1/lambda^2 aillar lambda
+
+# APARTAT A
+# P(T >= 13000) =  1 - P(T < 13000 = 1 - F(13000) = 1 - P(T<= 13000) = 1 - pexp
+
+1 - pexp(13000, 1/10000)
+
+#APARTAT B
+
+qexp(0.8, 1/10000)
+
+#APARTAT C
+set.seed(85)
+x <- rexp(100, 1/10000)
+
+# MEDIA
+mean(x)
+
+#MEDIANA
+median(x)
+
+#VARIANZA
+var(x)
 
